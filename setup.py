f12582888e131e599f13b0b0ebb39d40e04cf5bb
Ben Jones <ben@fogbutter.com>
2016-11-08 18:25:20 -0600

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
 
 config = {
-    'description': 'A nano framework for http requests proxied to AWS Lambda',
+    'description': 'Serverless nanoframework for AWS API Gateway and AWS Lambda',
+    'long_description': README,
     'author': 'Ben Jones',
     'author_email': 'ben@fogbutter.com',
-    'version': '0.1',
+    'version': '0.1.0',
     'packages': ['sippycup'],
     'name': 'sippycup',
-    'install_requires': ['Werkzeug']
+    'install_requires': ['Werkzeug'],
+    'license': 'MIT',
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development'
+    ],
+    'keywords': 'aws lambda api serverless',
+    'extras_require': {
+        'test': ['tox', 'pytest'],
+    },
+
 }
 
 setup(**config)
