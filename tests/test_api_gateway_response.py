938f012f0c98a237d26240c7f820abda2e7b5602
Ben Jones <ben@fogbutter.com>
2016-11-09 20:24:52 -0600

    Fix tests to reflect newly consolidated response object

--- tests/test_api_gateway_response.py
+++ tests/test_api_gateway_response.py
@@ -1,39 +1,28 @@
-from werkzeug.wrappers import Response
-from sippycup.responses import SippyCupApiGatewayResponse
+from sippycup.responses import SippyCupResponse
 
-r = Response()
+r = SippyCupResponse()
 r.status_code = 200
+r.headers['Content-Type'] = 'text/plain'
 r.set_data('I am here')
 r.set_cookie('ohai', 'there')
-r.headers['Content-Type'] = 'text/plain'
-sr = SippyCupApiGatewayResponse(r)
+
 
 
 def test_response_body():
-    assert sr.body == 'I am here'
+    assert r.apigr['body'] == 'I am here'
 
 
 def test_response_status():
-    assert sr.statusCode == 200
+    assert r.apigr['statusCode'] == 200
 
 
 def test_response_headers():
-    assert type(sr.headers) is dict
-    assert 'Content-Type' in sr.headers
-    assert sr.headers['Content-Type'] =='text/plain'
+    assert type(r.apigr['headers']) is dict
+    assert 'Content-Type' in r.headers
+    assert r.apigr['headers']['Content-Type'] =='text/plain'
 
 
 def test_response_cookies():
-    print(sr.headers['Set-Cookie'])
-    assert 'Set-Cookie' in sr.headers
-    assert sr.headers['Set-Cookie'] == 'ohai=there; Path=/'
-
-
-def test_response_as_dict():
-    sd = dict(sr)
-    assert 'headers' in sd
-    assert sd['headers']['Content-Type'] == 'text/plain'
-    assert 'body' in sd
-    assert sd['body'] == 'I am here'
-    assert 'statusCode' in sd
-    assert sd['statusCode'] == 200
+    print(r.apigr['headers']['Set-Cookie'])
+    assert 'Set-Cookie' in r.apigr['headers']
+    assert r.apigr['headers']['Set-Cookie'] == 'ohai=there; Path=/'
